b1dfb265235f1b5fa148d570c01390294bdf9d7d
gpdnjs70 <kshn0710@naver.com>
2026-07-09 21:46:06 +0900

    Board 수정
    - BlockData 생성
    - BlockData로 보드 생성하도록 변경
    - MainBoard인 경우에 타일 프리팹을 생성하도록 구현

---
+++ Assets/Scripts/BoardData/BoardData.cs
@@ -0,0 +1,33 @@
+// 강혜원 작성
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BoardType
+{
+    Main,       // 게임 보드
+    Background  // 배경 보드
+}
+
+// 특수 좌석 설정을 위한 구조체
+[System.Serializable]
+public struct SpecialSeat
+{
+    public Vector2Int gridIndex;       // 좌석의 (X, Y) 인덱스 위치
+    public PassengerType requiredType; // 이 자리에 앉아야 하는 승객 타입
+}
+
+[CreateAssetMenu(fileName = "BoardData", menuName = "Scriptable Objects/BoardData")]
+public class BoardData : ScriptableObject
+{
+    public float gridSize = 1.32f;
+
+    public BoardType type;
+    public string[] boardShape; // O, X로 이루어진 보드 모양
+    public Vector2 boardOffset = Vector2.zero;
+
+    public List<SpecialSeat> specialSeats = new List<SpecialSeat>();
+
+    public GameObject tilePrefab; // 게임 보드 타일 프리팹
+    public Vector2 tileOffset = Vector2.zero;
+}
